be36be97341671d878887f857906e004fcf38389
Domagoj Penic <dpenic23@gmail.com>
2017-02-04 02:22:10 +0100

    Implemented logic for viewing test files.

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,9 +1,11 @@
 package application;
 
 import java.io.File;
+import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
@@ -30,6 +32,18 @@ public class MainController {
 		chooseFile(textFieldAESKey);
 	}
 
+	public void viewFileAESInput(ActionEvent event) {
+		viewFile(textFieldAESInput);
+	}
+
+	public void viewFileAESOutput(ActionEvent event) {
+		viewFile(textFieldAESOutput);
+	}
+
+	public void viewFileAESKey(ActionEvent event) {
+		viewFile(textFieldAESKey);
+	}
+
 	/**
 	 * Open the file chooser and show the selected file in the specified text
 	 * field. If there is no selected file, show nothing.
@@ -46,6 +60,39 @@ public class MainController {
 
 		if (selectedFile != null) {
 			textField.setText(selectedFile.getAbsolutePath());
+		}
+	}
+
+	/**
+	 * Takes the absolute path from the specified text field and tries to open
+	 * it with the text editor depending on the operating system.
+	 * 
+	 * @param textField
+	 *            Text field with the absolute path.
+	 */
+	private void viewFile(TextField textField) {
+		String filePath = textField.getText().trim();
+		File file = new File(filePath);
+
+		boolean error = false;
+
+		if (!file.exists() || file.isDirectory() || !file.canRead()) {
+			error = true;
+		}
+
+		try {
+			if (!error) {
+				Runtime.getRuntime().exec("gedit " + filePath);
+			}
+		} catch (IOException e) {
+			error = true;
+		}
+
+		if (error) {
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			// alert.setTitle("Information Dialog");
+			alert.setHeaderText("Selected file cannot be opened!");
+			alert.show();
 		}
 	}
 
