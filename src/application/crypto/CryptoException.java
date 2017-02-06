69cdf0840da31c9d676ca5ca818bbff9e51d97af
Domagoj Penic <dpenic23@gmail.com>
2017-02-06 06:35:07 +0100

    Created an exception for cryptography operations.

---
+++ src/application/crypto/CryptoException.java
@@ -0,0 +1,12 @@
+package application.crypto;
+
+public class CryptoException extends Exception {
+
+	/** Default serial version ID. */
+	private static final long serialVersionUID = 1L;
+
+	public CryptoException(String message) {
+		super(message);
+	}
+
+}
