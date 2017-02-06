cb5ec54c7d258dcbdd0da2b75ac0b9343dc6b17d
Domagoj Penic <dpenic23@gmail.com>
2017-02-06 18:08:41 +0100

    Added default constructor.

--- src/application/crypto/CryptoException.java
+++ src/application/crypto/CryptoException.java
@@ -5,6 +5,9 @@ public class CryptoException extends Exception {
 	/** Default serial version ID. */
 	private static final long serialVersionUID = 1L;
 
+	public CryptoException() {
+	}
+
 	public CryptoException(String message) {
 		super(message);
 	}
