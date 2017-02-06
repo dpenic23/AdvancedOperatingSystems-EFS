96df4e6f233912c43951a5e474c7da7604f9dde1
Domagoj Penic <dpenic23@gmail.com>
2017-02-06 02:12:34 +0100

    Created class for key-value property encapsulation.

---
+++ src/application/file/Pair.java
@@ -0,0 +1,21 @@
+package application.file;
+
+public class Pair {
+
+	private String key;
+	private String value;
+
+	Pair(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+}
