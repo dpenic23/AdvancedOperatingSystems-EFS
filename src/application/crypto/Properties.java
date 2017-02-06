9062da5a015909c2eb023211bde53a7ec7a5a11a
Domagoj Penic <dpenic23@gmail.com>
2017-02-06 05:21:13 +0100

    List of properties changed from enumeration into list of public static variables.

--- src/application/crypto/Properties.java
+++ src/application/crypto/Properties.java
@@ -1,30 +1,19 @@
 package application.crypto;
 
-public enum Properties {
+public class Properties {
 
-	DESCRIPTION("Description"), 
-	SECRET_KEY("Secret key"), 
-	FILE_NAME("File name"), 
-	METHOD("Method"), 
-	KEY_LENGTH("Key length"), 
-	INIT_VECTOR("Initialization vector"), 
-	MODULUS("Modulus"), 
-	PUBLIC_EXP("Public exponent"), 
-	PRIVATE_EXP("Private exponent"), 
-	SIGNATURE("Signature"), 
-	DATA("Data"), 
-	ENVELOPE_DATA("Envelope data"), 
-	ENVELOPE_CRYPT_KEY("Envelope crypt key");
-
-	private final String text;
-
-	private Properties(final String text) {
-		this.text = text;
-	}
-
-	@Override
-	public String toString() {
-		return text;
-	}
+	public static final String DESCRIPTION = "Description";
+	public static final String SECRET_KEY = "Secret key";
+	public static final String FILE_NAME = "File name";
+	public static final String METHOD = "Method";
+	public static final String KEY_LENGTH = "Key length";
+	public static final String INIT_VECTOR = "Initialization vector";
+	public static final String MODULUS = "Modulus";
+	public static final String PUBLIC_EXP = "Public exponent";
+	public static final String PRIVATE_EXP = "Private exponent";
+	public static final String SIGNATURE = "Signature";
+	public static final String DATA = "Data";
+	public static final String ENVELOPE_DATA = "Envelope data";
+	public static final String ENVELOPE_CRYPT_KEY = "Envelope crypt key";
 
 }
